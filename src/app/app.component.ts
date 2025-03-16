3cc87fb1c0a26cf6431118eccc8cd3e6ceb90ecd
alodev <brayan.quialo@gmail.com>
2025-03-16 17:12:00 -0600

    feat(app): Add auth state manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthStore } from './store/auth.store';
+import { LocalStorageService } from '@shared/services/local-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'revistland';
+  private readonly store = inject(AuthStore);
+  private readonly localStorageService = inject(LocalStorageService);
+
+  constructor() {
+    effect(() => {
+      const email = this.store.email()
+      const accessToken = this.store.accessToken()
+      this.localStorageService.saveState({ email, accessToken });
+    });
+  }
 }
---
+++ src/app/store/auth.store.ts
@@ -0,0 +1,19 @@
+import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { Auth } from './models/auth-store.model';
+import { inject } from '@angular/core';
+import { LocalStorageService } from '@shared/services/local-storage.service';
+
+export const INITIAL_STATE: Auth = {
+    accessToken: '',
+    email: ''
+};
+
+export const AuthStore = signalStore(
+    { providedIn: 'root' },
+    withState((localStorageService = inject(LocalStorageService)) => localStorageService.getState()),
+    withMethods((store) => ({
+        changeEmail(email: string): void {
+            patchState(store, (state) => ({ ...state, email }));
+        }
+    }))
+);
