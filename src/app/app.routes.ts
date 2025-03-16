0e2d5b4ddf2a25b1c2d31f1a4dd9d11813abde8d
alodev <brayan.quialo@gmail.com>
2025-03-16 15:17:46 -0600

    feat(app): config routes for auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'welcome',
+        loadComponent: () => import('./shared/pages/landing/landing.component')
+    },
+    {
+        path: '',
+        redirectTo: 'welcome',
+        pathMatch: 'full'
+    }
+];
