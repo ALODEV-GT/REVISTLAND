eeca9834067148db6e23f14fb286d3e0f793afe5
alodev <brayan.quialo@gmail.com>
2025-03-16 15:13:50 -0600

    feat(auth): Add confirmation account component

---
+++ src/app/shared/modules/auth/components/confirmation/confirmation.component.ts
@@ -0,0 +1,55 @@
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthPage } from '@shared/modules/auth/models/auth-control-page';
+import { AuthService } from '@shared/services/auth.service';
+import { Confirmation } from '../../models/auth';
+
+@Component({
+  selector: 'app-confirmation',
+  imports: [ReactiveFormsModule],
+  templateUrl: './confirmation.component.html',
+  styleUrl: './confirmation.component.scss'
+})
+export class ConfirmationComponent {
+  @Output() changePage = new EventEmitter<AuthPage>();
+
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
+
+  errorMessage: string = '';
+
+  confirmationForm: FormGroup = this.formBuilder.group({
+    code: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(6)]],
+  });
+
+
+  confirm() {
+    this.errorMessage = '';
+
+    if (this.confirmationForm.invalid) {
+      this.errorMessage = 'Por favor, ingrese el codigo de confirmación';
+      return
+    }
+
+    const confirmation: Confirmation = this.confirmationForm.getRawValue();
+
+    this.authService.confirmation(confirmation).subscribe({
+      next: () => {
+
+      },
+      error: (error) => {
+        this.handleErrorConfirmation(error);
+      }
+    })
+  }
+  
+  handleErrorConfirmation(error: any) {
+
+  }
+
+  toLogin() {
+    this.changePage.emit(AuthPage.LOGIN);
+  }
+}
