0b1bc3cd734e1ca7abd1cf72695dd99bc6f51ea7
Dylan <dylanelias201931369@cunoc.edu.gt>
2025-03-22 00:23:51 -0600

    refactor(alerts): move alerts from app its own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,18 @@
-import { CommonModule } from '@angular/common';
 import { Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LocalStorageService } from '@shared/services/local-storage.service';
+import { AlertsComponent } from './shared/components/alerts/alerts.component';
 import { AuthStore } from './store/auth.store';
-import { AlertStore } from './store/alert.store';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet, AlertsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   private readonly store = inject(AuthStore);
   private readonly localStorageService = inject(LocalStorageService);
-
-  readonly alertStore = inject(AlertStore);
 
   constructor() {
     effect(() => {
