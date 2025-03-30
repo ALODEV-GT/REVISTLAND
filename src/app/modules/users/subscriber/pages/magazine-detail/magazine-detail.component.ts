c378e3f380a165dd7778260f4d4a7172a491f7c7
alodev <brayan.quialo@gmail.com>
2025-03-29 21:56:46 -0600

    feat(ads): Add ads in home and my subscriptions

--- src/app/modules/users/subscriber/pages/magazine-detail/magazine-detail.component.ts
+++ src/app/modules/users/subscriber/pages/magazine-detail/magazine-detail.component.ts
@@ -11,110 +11,130 @@ import { IssuesService } from '@shared/modules/editor/services/issues.service';
 import { Issue } from '@shared/modules/editor/models/magazine.model';
 import { CategoryService } from '@shared/modules/editor/services/category.service';
 import { SubscriptionService } from '@shared/modules/editor/services/subscription.service';
-
+import { AdUserService } from '@shared/modules/editor/services/ad-user.service';
+import { AdUser, AdViewCreateDto } from '@shared/modules/editor/models/adUser';
+import { SafeUrlPipe } from '@shared/utils/SafeUrlPipe';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-magazine-detail',
-  imports: [LucideAngularModule, ReactiveFormsModule, CommonModule],
+  imports: [LucideAngularModule, ReactiveFormsModule, CommonModule, SafeUrlPipe],
   templateUrl: './magazine-detail.component.html',
   styleUrl: './magazine-detail.component.scss'
 })
 export default class MagazineDetailComponent implements OnInit {
-  private readonly magazineService = inject(MagazineService)
-  private readonly likeService = inject(LikeService)
-  private readonly commentService = inject(CommentService)
-  private readonly issueService = inject(IssuesService)
-  readonly categoryService = inject(CategoryService)
-  readonly subscriptionService = inject(SubscriptionService)
+  private readonly magazineService = inject(MagazineService);
+  private readonly likeService = inject(LikeService);
+  private readonly commentService = inject(CommentService);
+  private readonly issueService = inject(IssuesService);
+  private readonly adUserService = inject(AdUserService);
+  readonly categoryService = inject(CategoryService);
+  readonly subscriptionService = inject(SubscriptionService);
 
-  readonly Heart = Heart
-  activateRoute = inject(ActivatedRoute)
-  liked: boolean = false
-  subscribed: boolean = false
-  comment: FormControl = new FormControl("", [Validators.required, Validators.minLength(2)])
+  readonly Heart = Heart;
+  activateRoute = inject(ActivatedRoute);
+  liked: boolean = false;
+  subscribed: boolean = false;
+  comment: FormControl = new FormControl("", [Validators.required, Validators.minLength(2)]);
 
-  magazine: MagazineItem | null = null
-  comments: Comment[] = []
-  issues: Issue[] = []
+  magazine: MagazineItem | null = null;
+  comments: Comment[] = [];
+  issues: Issue[] = [];
+  leftAds: AdUser[] = [];
+  rightAds: AdUser[] = [];
+  allAds: AdUser[] = [];
 
   ngOnInit(): void {
     const id = this.activateRoute.snapshot.paramMap.get('id');
     this.magazineService.getMagazine(id!).subscribe({
       next: (resp: MagazineItem) => {
-        this.magazine = resp
-        this.loadComments()
-        this.loadIssues()
+        this.magazine = resp;
+        this.loadComments();
+        this.loadIssues();
       }
-    })
+    });
 
     this.likeService.userLikedMagazine(id!).subscribe({
       next: (resp: boolean) => {
-        this.liked = resp
+        this.liked = resp;
       }
-    })
+    });
 
     this.subscriptionService.userSubscribedMagazine(id!).subscribe({
       next: (resp: boolean) => {
-        this.subscribed = resp
+        this.subscribed = resp;
       }
-    })
+    });
+
+    const adRequests = Array(6).fill(null).map(() => this.adUserService.getRandomAd());
+    forkJoin(adRequests).subscribe(ads => {
+      this.leftAds = ads.slice(0, 3);
+      this.rightAds = ads.slice(3, 6);
+      this.allAds = ads.slice(0, 2);
+
+      ads.forEach(ad => {
+        const view: AdViewCreateDto = { adId: ad.id, urlView: window.location.href };
+        this.adUserService.saveView(view).subscribe();
+      });
+    });
   }
+
 
   loadIssues() {
     this.issueService.getIssues(this.magazine?.id!).subscribe({
       next: (resp: Issue[]) => {
-        this.issues = resp
+        this.issues = resp;
       }
-    })
+    });
   }
 
   togglelike() {
     if (this.magazine?.disableLikes) {
-      return
+      return;
     }
 
     if (this.liked) {
       this.likeService.unlikeMagazine(this.magazine!.id).subscribe({
         next: () => {
-          this.magazine!.likeCount -= 1
-          this.liked = false
+          this.magazine!.likeCount -= 1;
+          this.liked = false;
         }
-      })
+      });
     } else {
       this.likeService.likeMagazine(this.magazine!.id).subscribe({
         next: () => {
-          this.magazine!.likeCount += 1
-          this.liked = true
+          this.magazine!.likeCount += 1;
+          this.liked = true;
         }
-      })
+      });
     }
   }
 
   toggleSubscription() {
     if (this.magazine?.disableSuscriptions) {
-      //TODO: Mostrar erro que no se puede suscribir
-      return
+      // TODO: Mostrar error que no se puede suscribir
+      return;
     }
 
     if (this.subscribed) {
       this.subscriptionService.unsubscribeMagazine(this.magazine!.id).subscribe({
         next: () => {
-          this.subscribed = false
+          this.subscribed = false;
         }
-      })
+      });
     } else {
       this.subscriptionService.subscribeMagazine(this.magazine!.id).subscribe({
         next: () => {
-          this.subscribed = true
+          this.subscribed = true;
         }
-      })
+      });
     }
   }
 
   saveComment() {
     if (this.comment.invalid) {
-      //TODO: Mostrar errro de que es un comentario invalido
-      return
+      // TODO: Mostrar error de que es un comentario inválido
+      return;
     }
 
     this.commentService.comment(this.magazine!.id, this.comment.value).subscribe({
@@ -122,15 +142,15 @@ export default class MagazineDetailComponent implements OnInit {
         this.comments.unshift(resp);
         this.comment.reset();
       }
-    })
+    });
   }
 
   loadComments() {
     this.commentService.getCommentsMagazine(this.magazine!.id).subscribe({
       next: (resp: Comment[]) => {
-        this.comments = resp
+        this.comments = resp;
       }
-    })
+    });
   }
 
   openPdfInNewTab(pdfUrl: string): void {
@@ -140,5 +160,4 @@ export default class MagazineDetailComponent implements OnInit {
     }
     window.open(pdfUrl, '_blank');
   }
-
 }
--- src/app/modules/users/subscriber/pages/my-subscriptions/my-subscriptions.component.ts
+++ src/app/modules/users/subscriber/pages/my-subscriptions/my-subscriptions.component.ts
@@ -4,30 +4,49 @@ import { CategoryService } from '@shared/modules/editor/services/category.servic
 import { MagazineService } from '@shared/modules/editor/services/magazine.service';
 import { MagazineItem } from '@subscriber/models/magazine';
 import { Heart, LucideAngularModule } from 'lucide-angular';
+import { AdUserService } from '@shared/modules/editor/services/ad-user.service';
+import { AdUser, AdViewCreateDto } from '@shared/modules/editor/models/adUser';
+import { SafeUrlPipe } from '@shared/utils/SafeUrlPipe';
+import { forkJoin } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-subscriptions',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, CommonModule, SafeUrlPipe],
   templateUrl: './my-subscriptions.component.html',
   styleUrl: './my-subscriptions.component.scss'
 })
 export default class MySubscriptionsComponent implements OnInit {
-  readonly Heart = Heart
-  readonly categoryService = inject(CategoryService)
-  private readonly magazineService = inject(MagazineService)
-  private readonly router = inject(Router)
-  magazines: MagazineItem[] = []
+  readonly Heart = Heart;
+  readonly categoryService = inject(CategoryService);
+  private readonly magazineService = inject(MagazineService);
+  private readonly router = inject(Router);
+  private readonly adUserService = inject(AdUserService);
+  magazines: MagazineItem[] = [];
+  leftAds: AdUser[] = [];
+  rightAds: AdUser[] = [];
+  allAds: AdUser[] = [];
 
   ngOnInit(): void {
     this.magazineService.getUserMagazines().subscribe({
       next: (resp: MagazineItem[]) => {
-        this.magazines = resp
+        this.magazines = resp;
       }
-    })
+    });
+
+    const adRequests = Array(8).fill(null).map(() => this.adUserService.getRandomAd());
+    forkJoin(adRequests).subscribe(ads => {
+      this.leftAds = ads.slice(0, 3);
+      this.rightAds = ads.slice(3, 6);
+      this.allAds = ads.slice(6, 8);
+      ads.forEach(ad => {
+        const view: AdViewCreateDto = { adId: ad.id, urlView: window.location.href };
+        this.adUserService.saveView(view).subscribe();
+      });
+    });
   }
 
   seeMagazineDetail(idMagazine: number) {
-    this.router.navigate([`rl`, idMagazine])
+    this.router.navigate([`rl`, idMagazine]);
   }
-
 }
