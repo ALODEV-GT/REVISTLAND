4c30f6afb1ae1e3407312a5e25dfb2762c1a52cf
Elvis Aguilar <elvisaguilar201930304@cunoc.edu.gt>
2025-03-31 22:55:22 -0600

    add profile and logout to admin and announcer

--- src/app/shared/modules/announcer/components/navbar/navbar.component.ts
+++ src/app/shared/modules/announcer/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthStore } from 'app/store/auth.store';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  private readonly route = inject(Router)
+  store = inject(AuthStore);
+  id = this.store.session().id.toString()
+  profilePicture = this.store.session().profilePicture
+
+
+  ngOnInit(){
+    if (!this.profilePicture) {
+      this.profilePicture = 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+    }
+  }
+
+  goProfile(){
+    const id = this.store.session().id.toString()
+    this.route.navigate([`profile/${id}`])
+  }
 
 }
